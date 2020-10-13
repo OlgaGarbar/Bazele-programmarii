e21fbd8507caafca06fb7043fd2e6f12dfe26a5e
OlgaGarbar <71259669+OlgaGarbar@users.noreply.github.com>
2020-10-13 20:49:33 +0300

    ia popitalasi ee zaconciti. No uvi///

--- 05.10.2020 ghjuh.pas
+++ 05.10.2020 ghjuh.pas
@@ -1,17 +1,18 @@
-﻿Program ora_finisării_lecției;
-Const L=45;
-Const R1,R3=15;
-Const I=8;
+﻿Program ora_finisării_lecții;
 Var
   NL:Word;
   OT:word;
+  R:word;
+  OI:word;
 Begin
   Write('Introduceti numarul de lectii  ');
   Read (NL);
-  NL:=L*NL;
-  
-  
-  OT:= 
+  Write('Introduceti numerul de recreatii  ');
+  Read(R);
+  NL:=NL*45+(R*10);
+  Write('Introduceti ora inceputul lectii ');
+  Read(OI);
+  OT:= OI+NL; 
 end.
   
   

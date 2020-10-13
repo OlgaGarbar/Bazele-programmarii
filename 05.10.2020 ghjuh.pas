703548d7eccf20bfeb36770bdf6e10f0da052a4a
OlgaGarbar <mamapsih87@gmail.com>
2020-10-13 20:33:35 +0300

    moia programma pascal

---
+++ 05.10.2020 ghjuh.pas
@@ -0,0 +1,17 @@
+﻿Program ora_finisării_lecției;
+Const L=45;
+Const R1,R3=15;
+Const I=8;
+Var
+  NL:Word;
+  OT:word;
+Begin
+  Write('Introduceti numarul de lectii  ');
+  Read (NL);
+  NL:=L*NL;
+  
+  
+  OT:= 
+end.
+  
+  
